05cfe790b5d8b279df6fcbccaf13cbe4aa55c9ec
DivergenteNM <nicolasmdivergent@gmail.com>
2024-12-10 12:25:42 -0500

    prolog, front client components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  message = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api')
+      .subscribe(response => this.message = response.message);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -28,5 +30,86 @@ app.get('/api', (req, res) => {
     res.json({ message: '¡Hola desde el backend con Node.js y MySQL!' });
 });
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+//Prolog
+
+export class Prolog {
+    static prologFile = fs.readFileSync(path.join(__dirname, 'prolog.pl'), 'utf8');
+
+    constructor() {}
+
+    iniciarSesion() {
+        this.session = window.pl.create();
+    }
+
+    async obtenerRespuesta() {
+        const resultado = await this.ejecutarConsulta('regla(Respuesta).');
+        const respuestaString = resultado[0].match(/\[(.*?)\]/)[1];
+        const respuestasArray = respuestaString.split(',');
+
+        return respuestasArray;
+    }
+
+    async enviarRespuesta(respuesta) {
+        await this.ejecutarConsulta(`regla('${respuesta}').`);
+
+        const resultado = await this.ejecutarConsulta('regla(Respuesta).');
+        const respuestaString = resultado[0].match(/\[(.*?)\]/)[1];
+        const respuestasArray = respuestaString.split(',');
+
+        return respuestasArray;
+    }
+
+    ejecutarConsulta(regla) {
+        return new Promise((resolve, reject) => {
+            const session = this.session;
+            const consulta = regla || this.reglaPorDefecto;
+            let resultados = [];
+            let finalizado = false;
+
+            // Cargar el archivo del proyecto Prolog
+            session.consult(Prolog.prologFile, {
+                success: () => {
+                    session.query(consulta, {
+                        success: () => {
+                            session.answers(
+                                (x) => {
+                                    let respuesta = session.format_answer(x);
+                                    if (respuesta) {
+                                        resultados.push(respuesta);
+                                    }
+                                },
+                                {
+                                    success: () => {
+                                        finalizado = true;
+                                        resolve(resultados);
+                                    },
+                                    error: (err) => {
+                                        finalizado = true;
+                                        reject(err);
+                                    }
+                                }
+                            );
+                        },
+                        error: (err) => {
+                            finalizado = true;
+                            reject(err);
+                        }
+                    });
+
+                    // Timeout
+                    setTimeout(() => {
+                        if (!finalizado) {
+                            resolve(resultados);
+                        }
+                    }, 1000);
+                },
+                error: (err) => {
+                    reject(err);
+                }
+            });
+        });
+    }
+}
